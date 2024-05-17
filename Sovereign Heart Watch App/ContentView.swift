86589b1a140395294f8bb7ab0a30d3532a63ad8c
danieliank <daxter1835@gmail.com>
2024-05-17 19:33:42 +0700

    Initial Commit

---
+++ Sovereign Heart Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sovereign Heart Watch App
+//
+//  Created by Daniel Ian on 17/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sovereign Heart Watch App/Sovereign_HeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sovereign_HeartApp.swift
+//  Sovereign Heart Watch App
+//
+//  Created by Daniel Ian on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Sovereign_Heart_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
